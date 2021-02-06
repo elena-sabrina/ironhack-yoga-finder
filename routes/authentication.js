7044e3391b219a7507fcca59e24c06a55bc970d6
Elena Hadjimina <hadjimina.elena@gmail.com>
2021-02-06 17:43:54 +0800

    basic layout done working on form styling

--- routes/authentication.js
+++ routes/authentication.js
@@ -11,6 +11,13 @@ const router = new Router();
 //STORAGE CLOUDINARY
 const uploadMiddleware = require('./../middleware/file-upload');
 
+//MATERIAL IO
+//npm install @material/textfield
+import {MDCTextField} from '@material/textfield';
+const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
+
+
+
 //ROUTES
 
 router.get('/sign-up', (req, res, next) => {
