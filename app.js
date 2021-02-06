19f1a063d07616e4f621b0f5e2eeba2fb65d9811
Elena Hadjimina <hadjimina.elena@gmail.com>
2021-02-06 20:56:12 +0800

    tried to add burgermenu. failed to add js

--- app.js
+++ app.js
@@ -19,6 +19,17 @@ const teacherRouter = require('./routes/teacher');
 
 const app = express();
 
+//HAMBURGER
+/*
+const hamburger = document.querySelector('hamburger');
+const navLinks = document.querySelector('nav-links');
+const links = document.querySelectorAll('nav-links li');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+});
+*/
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
