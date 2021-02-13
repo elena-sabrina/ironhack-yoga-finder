5187568c88e2be14b1058d0f83f2bc3baa7df8ed
Elena Hadjimina <hadjimina.elena@gmail.com>
2021-02-13 22:45:07 +0800

    working on editing profile.issues with name in findbyidanduptade function

--- routes/profile.js
+++ routes/profile.js
@@ -8,13 +8,32 @@ const User = require('../models/user');
 const Class = require('../models/class');
 
 router.get('/', routeGuard, (req, res, next) => {
+  const userId = req.session.userId;
+
   Class.find()
     .then((classes) => {
       console.log('hello classes');
-
-      res.render('profile/teacher', { classes });
+      res.render('profile/teacher', { classes, userId });
     })
     .catch((error) => {
+      next(error);
+    });
+});
+
+router.get('/:id/edit', routeGuard, (req, res, next) => {
+  const id = req.params.id;
+
+  User.findByIdAndUpdate(id, {
+    name
+    email,
+    passwordHashAndSalt: passwordHashAndSalt,
+    picture
+  })
+    .then((users) => {
+      res.render('profile/edit');
+    })
+    .catch((error) => {
+      console.log(error);
       next(error);
     });
 });
